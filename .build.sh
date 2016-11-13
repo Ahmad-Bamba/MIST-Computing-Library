1a5eb9b2a9b11dbfff37acc2f53f52d61f125853
usb <ahmad.eb.bamba@gmail.com>
2016-11-13 18:39:55 -0500

    Changed echo to be more useful

--- .build.sh
+++ .build.sh
@@ -4,5 +4,5 @@ if [ ! -d "$DIRECTORY" ]; then
     wget "https://sourceforge.net/projects/asio/files/latest/download?source=files"
     tar -xvf "download?source=files"
 fi
-echo g++ -std=c++1y -Iasio-1.10.8/include/ src/*.cpp -o out
+echo "Building..."
 g++ -std=c++1y -Iasio-1.10.8/include/ -c -lpthread src/*.cpp -o a.o
