797c94dcaf8449141f6f3afc9b93ebb0c429993d
usb <ahmad.eb.bamba@gmail.com>
2016-11-14 15:19:46 -0500

    More reliable downloading plus printf

--- .build.sh
+++ .build.sh
@@ -1,8 +1,9 @@
 VERSION="1.10.8"
 DIRECTORY="asio-$VERSION"
 if [ ! -d "$DIRECTORY" ]; then
-    wget "https://sourceforge.net/projects/asio/files/latest/download?source=files"
-    tar -xvf "download?source=files"
+    wget "http://learn612.000webhostapp.com/asio-1.10.8.tar.gz"
+    tar -xvf "asio-1.10.8.tar.gz"
 fi
-echo "Building..."
+printf "\nBuilding...\n"
 g++ -std=c++0x -Iasio-1.10.8/include/ -c -lpthread src/*.cpp -o a.o
+printf "Exited with code $?\n"
