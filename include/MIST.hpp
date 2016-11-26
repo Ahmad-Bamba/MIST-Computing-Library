76420da2d61514836925b180578d807bff45a43c
Mynameisspam1 <Mynameisspam1@gmail.com>
2016-11-26 18:19:01 -0500

    Fleshed out local initialization stuff

--- include/MIST.hpp
+++ include/MIST.hpp
@@ -16,7 +16,6 @@ namespace MIST {
 		{
 			unsigned long allowedThreads;
 			unsigned long long allowedMemory;
-			std::string name;
 			bool enableHT;
 		};
 
@@ -25,10 +24,10 @@ namespace MIST {
 			bool isLocal; //is it this computer?
 			std::string address; //what address is it? "local" for if it's local
 			computerHardware computer; //What hardware does it have?
+            std::string name; //name of the computer to MIST network
 		};
 
-		computerHardware thisComputer; //this computer
-		MISTHost local; //this computer
+        MISTHost thisComputer; //this computer
 
         std::vector<MISTHost> Hosts;
         std::shared_ptr<Scheduler> scheduler;
@@ -47,31 +46,36 @@ namespace MIST {
         ~MIST() = default;
 		//LOCAL INITIALIZATION
 		void InitComputer(unsigned long threads = 1, unsigned long long memory = 2048, std::string name = "Unnamed, Unloved Computer", std::string address = "0.0.0.0", bool enableHT = false) {
-
+            thisComputer.isLocal = true;
+            thisComputer.address = "local";
+            thisComputer.computer.allowedMemory = memory;
+            thisComputer.computer.allowedThreads = threads;
+            thisComputer.name = name;
+            thisComputer.computer.enableHT = enableHT;
 		}
 		void setThreads(unsigned long threads) {
-
+            thisComputer.computer.allowedThreads = threads;
 		}
 		void setMemory(unsigned long long memory) {
-
+            thisComputer.computer.allowedMemory = memory;
 		}
 		void enableHT(bool enableHT) {
-
+            thisComputer.computer.enableHT = enableHT;
 		}
 		void setName(std::string computerName) {
-
+            thisComputer.name = computerName;
 		}
 		void setAddress(std::string address) {
-
+            thisComputer.address = address;
 		}
 
 		//NETWORK INITIALIZATION
-        void addMachine(std::string IP) {
-            //IPs.push_back(IP);
+        void inviteMachine(std::string IP) { //invites machine to network (changed from addMachine to signify that only master machine can do this)
+            //Perhaps accomplish this by creating an invite task in MIST.hpp that sends information back to this machine?
         }
 
-  /*      void sendTask(std::shared_ptr<Task<auto>> task) {
-            send(task.getID());
+  /*    void sendTask(std::shared_ptr<Task<auto>> task) {
+           send(task.getID());
         }*/
     };
 }
