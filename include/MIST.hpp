d4bcc3605ff704be8854a645c8a55ff2bfa01c12
Mynameisspam1 <Mynameisspam1@gmail.com>
2016-11-26 17:40:47 -0500

    Made some minor syntax fixes. Commented out any mention of the type auto in MIST.hpp in order to make it compile in MSVC. Also added some functions from my (now defunct) local repository.

--- include/MIST.hpp
+++ include/MIST.hpp
@@ -16,9 +16,9 @@ namespace MIST {
         std::shared_ptr<Scheduler> scheduler;
         bool is_master;
 
-        void send(auto message) {
+        /*void send(auto message) {
             //send message using ASIO
-        }
+        }*/
 
     public:
         MIST(std::vector<std::string> IPs, bool is_master) {
@@ -28,28 +28,26 @@ namespace MIST {
         }
 
         ~MIST() = default;
-
-		void MIST::InitComputer(unsigned long threads, unsigned long long memory, std::string name, std::string address, bool enableHT)
+		//LOCAL INITIALIZATION
+		void InitComputer(unsigned long threads, unsigned long long memory, std::string name, std::string address, bool enableHT)
 		{
-			std::cout << "Threads allowed:" << threads << std::endl;
-			std::cout << "Memory allowed:" << memory << std::endl;
-			if (enableHT)
-			{
-				std::cout << "HT enabled" << std::endl;
-			}
-			else {
-				std::cout << "HT disabled" << std::endl;
-			}
-			std::cout << name << std::endl;
-			std::cout << address << std::endl;
+
 		}
 
+		void InitComputer(unsigned long threads = 1, unsigned long long memory = 2048, std::string name = "UnnamedComputer UnlovedComputer", std::string address = "0.0.0.0", bool enableHT = false);
+		void setThreads(unsigned long threads);
+		void setMemory(unsigned long long memory);
+		void enableHT(bool enableHT);
+		void setName(std::string computerName);
+		void setAddress(std::string address);
+
+		//NETWORK INITIALIZATION
         void addMachine(std::string IP) {
             IPs.push_back(IP);
         }
 
-        void sendTask(std::shared_ptr<Task<auto>> task) {
+  /*      void sendTask(std::shared_ptr<Task<auto>> task) {
             send(task.getID());
-        }
+        }*/
     };
 }
