b666f7808a78cdaa4d7b57e48ece865a68a5f622
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2016-12-25 00:26:32 -0500

    Simple sending and receiving of data works

--- src/MIST.cpp
+++ src/MIST.cpp
@@ -8,7 +8,7 @@
 int main() {
     MIST::ReceiveData receiveObj;
 
-    std::string s = receiveObj.receive<8>();
+    std::string s = receiveObj.receive<64>();
     std::cout << "Message received: '" << s << "'" << std::endl;
 
     return 0;
