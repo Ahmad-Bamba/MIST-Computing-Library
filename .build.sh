9b62e0e6b9cdbcadea9af08b55e3a750c6539d46
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-03-31 14:30:33 -0400

    Rebuild protobuf files with script

--- .build.sh
+++ .build.sh
@@ -5,6 +5,11 @@ if [ ! -f /usr/local/lib/libprotobuf.so ]; then
     ./.protobuf_dl.sh
 fi
 
+# this should be false on users, but true on travis
+if [ ! -f protobuf_files/MIST.pb.h ]; then
+    protoc -I=protobuf_files/ --cpp_out=protobuf_files/ protobuf_files/MIST.proto
+fi
+
 if [ ! -d "$DIRECTORY" ]; then
     wget "http://learn612.000webhostapp.com/asio-1.10.8.tar.gz"
     tar -xvf "asio-1.10.8.tar.gz"
