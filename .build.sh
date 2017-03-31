66b5cd3060d9103456bdb0678dec793d2aa7e86e
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-03-31 18:46:32 -0400

    Make .protobuf_dl.sh runnable

--- .build.sh
+++ .build.sh
@@ -5,9 +5,12 @@ if [ ! -f /usr/local/lib/libprotobuf.so ]; then
     ./.protobuf_dl.sh
 fi
 
-echo "MIST.pb files not found. Generating from protoc..."
-protoc -I=protobuf_files/ --cpp_out=protobuf_files/ protobuf_files/MIST.proto
-echo "protoc exited with code: $?"
+# this should be false on users, but true on travis
+if [ ! -f protobuf_files/MIST.pb.h ]; then
+    echo "MIST.pb files not found. Generating from protoc..."
+    protoc -I=protobuf_files/ --cpp_out=protobuf_files/ protobuf_files/MIST.proto
+    echo "protoc exited with code: $?"
+fi
 
 if [ ! -d "$DIRECTORY" ]; then
     wget "http://learn612.000webhostapp.com/asio-1.10.8.tar.gz"
