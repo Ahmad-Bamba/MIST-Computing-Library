8a9930564fa5f25a29cb7aacd1efbc87717d188f
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-04-24 10:50:04 -0400

    Receive implemented for windows

--- include/networking/Receive.hpp
+++ include/networking/Receive.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <tuple>
+#include <cassert>
+#include <sstream>
 
 #ifndef WIN32
 
@@ -24,8 +26,9 @@ public:
         m_address.sin_port = htons(m_port);
         m_address.sin_addr.s_addr = INADDR_ANY;
 
-        if(m_sock < 0)
-            std::cerr << "Receive.h: Failed to open socket at line " << __LINE__ << std::endl;
+        std::stringstream ss;
+        ss << "Receive.h: Failed to open socket at line " << __LINE__ << std::endl;
+        assert(m_sock >= 0 && ss.str().c_str());
     }
 
     ReceiveData(const ReceiveData& from) = delete;
@@ -34,10 +37,10 @@ public:
     inline bool establish() {
         if (bind(m_sock, (struct sockaddr *) &m_address, sizeof(m_address)) < 0) {
             std::cerr << "Receive.h: failed to bind port at line " << __LINE__ << std::endl;
-            return false;
+            return true;
         }
         listen(m_sock, 5);
-        return true;
+        return false;
     }
 
     inline std::string receive_chunk(size_t size) {
@@ -62,11 +65,95 @@ public:
 
 #else
 
-class ReceiveData() {
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+
+#include <memory>
+#include <exception>
+#include <string>
+
+class ReceiveData {
+    short m_port;
+    std::unique_ptr<WSADATA> m_wsa_data { nullptr };
+    // The pointer ruse was a distaction
+    addrinfo* m_result;
+    std::unique_ptr<addrinfo> m_ptr { nullptr };
+    addrinfo m_hints { }; //TODO make sure this actually works
+    SOCKET m_sock;
 public:
-    ReceiveData() = default;
+    ReceiveData(const unsigned short& port) {
+        m_port = port;
+
+        std::stringstream ss;
+        ss << "Receive.h: Failed to open socket at line " << __LINE__ << std::endl;
+        assert(WSAStartup(MAKEWORD(2, 2), m_wsa_data.get()) != 0 && ss.str().c_str());
+
+        m_hints.ai_family = AF_INET;
+        m_hints.ai_socktype = SOCK_STREAM;
+        m_hints.ai_protocol = IPPROTO_TCP;
+        m_hints.ai_flags = AI_PASSIVE;
+
+        if(getaddrinfo(nullptr, std::to_string(m_port).c_str(), &m_hints, &m_result) != 0) {
+            std::cerr << "getadderinfo failed at line " << __LINE__ << std::endl;
+            WSACleanup();
+            std::terminate();
+        }
+
+        m_sock = INVALID_SOCKET;
+        m_sock = socket(m_result->ai_family, m_result->ai_socktype, m_result->ai_protocol);
+    }
     ReceiveData(const ReceiveData& from) = delete;
     ReceiveData(ReceiveData&& move) = delete;
+    ~ReceiveData() {
+        if(m_result != nullptr)
+            delete m_result;
+        WSACleanup();
+    }
+
+    inline bool establish() {
+        if(bind(m_sock, m_result->ai_addr, static_cast<int>(m_result->ai_addrlen)) == SOCKET_ERROR) {
+            std::cerr << "Receive.h: bind failed with error " << WSAGetLastError() << " at line " << __LINE__ << std::endl;
+            closesocket(m_sock);
+            return true;
+        }
+        if (listen(m_sock, 5) == SOCKET_ERROR) {
+            std::cerr << "Receive.h: listen failed with error " << WSAGetLastError() << " at line " << __LINE__ << std::endl;
+            closesocket(m_sock);
+            return true;
+        }
+        delete m_result;
+        return false;
+    }
+
+    inline std::string receive_chunk(size_t size) {
+        char* buffer = new char[size];
+        SOCKET client;
+        client = accept(client, nullptr, nullptr);
+        if(client == INVALID_SOCKET) {
+            std::cerr << "Receive.h: accept failed with error " << WSAGetLastError() << " at line " << __LINE__ << std::endl;
+            closesocket(m_sock);
+            return "";
+        }
+        int i;
+        std::string ret;
+        //accept until connection closes
+        do {
+            i = recv(client, buffer, size, 0);
+            if(i > 0)
+                ret = std::string(buffer);
+        } while(i > 0);
+        return ret;
+    }
+
+    inline auto get_info() {
+        return std::tuple<unsigned short, int>(m_port, m_sock);
+    }
 };
 
 #endif // WIN32
--- include/networking/Send.hpp
+++ include/networking/Send.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <tuple>
+#include <cassert>
+#include <sstream>
 
 #ifndef WIN32
 
@@ -23,9 +25,9 @@ private:
         buffer = raw;
         if(write(m_sock, buffer, size + 1) < 0) {
             std::cerr << "Send.h: Failed to write data to socket at line " << __LINE__ << std::endl;
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 public:
     SendData(const std::string& ip, const unsigned short& port) {
@@ -33,14 +35,20 @@ public:
         m_ip = ip;
 
         m_sock = socket(AF_INET, SOCK_STREAM, 0);
-        if(m_sock < 0)
-            std::cerr << "Send.h: Error opening socket at line " << __LINE__ << std::endl;
+
+        std::stringstream ss;
+        ss << "Send.h: Error opening socket at line " << __LINE__ << std::endl;
+        assert(m_sock >= 0 && ss.str().c_str());
 
         m_address.sin_family = AF_INET;
         m_address.sin_port = htons(m_port);
+        
         if(inet_pton(AF_INET, m_ip.c_str(), &m_address.sin_addr))
             std::cerr << "Send.h Error getting set hostname (inet_pton) at line " << __LINE__ << std::endl;
     }
+
+    SendData(const SendData& from) = delete;
+    SendData(SendData&& move) = delete;
 
     inline bool establish() {
         if(connect(m_sock, (struct sockaddr *)&m_address, sizeof(m_address)) < 0) {
@@ -62,10 +70,35 @@ public:
 #else
 
 class SendData {
+private:
+    unsigned short m_port;
+    std::string m_ip;
+
+    inline bool send(const char* raw, size_t size) {
+        const char* buffer = new char[size];
+        return false;
+    }
 public:
-    SendData() = default;
+    SendData(const std::string& ip, const unsigned short& port) {
+        m_port = port;
+        m_ip = ip;
+    }
+
     SendData(const SendData& from) = delete;
     SendData(SendData&& move) = delete;
+
+    inline bool establish() {
+        return false;
+    }
+
+    inline bool send_string(std::string data) {
+        return send(data.c_str(), data.size());
+    }
+
+    inline auto get_info() {
+        return std::tuple<unsigned short, std::string, int>(m_port, "", 0);
+    }
 };
+
 
 #endif // WIN32
